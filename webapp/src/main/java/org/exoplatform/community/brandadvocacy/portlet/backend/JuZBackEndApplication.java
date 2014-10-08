8d8cfb5b8fc57e4b7543ce27c9de181da05cd327
anhvta <chun@Tuans-MacBook-Pro.local>
2014-10-08 07:32:33 +0700

    separate portlet , extension package

--- webapp/src/main/java/org/exoplatform/community/brandadvocacy/portlet/backend/JuZBackEndApplication.java
+++ webapp/src/main/java/org/exoplatform/community/brandadvocacy/portlet/backend/JuZBackEndApplication.java
@@ -2,6 +2,10 @@ package org.exoplatform.community.brandadvocacy.portlet.backend;
 
 import juzu.*;
 import juzu.template.Template;
+import org.exoplatform.brandadvocacy.model.Mission;
+import org.exoplatform.brandadvocacy.service.IService;
+import org.exoplatform.services.organization.OrganizationService;
+
 import javax.inject.Inject;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -11,43 +15,38 @@ import java.util.Set;
 /**
  * Created by exoplatform on 01/10/14.
  */
-
+@SessionScoped
 public class JuZBackEndApplication {
 
-  static Set<String> locations = new HashSet<String>();
-
-  static {
-    locations.add("hanoi");
-    locations.add("paris");
-  }
   @Inject
   @Path("index.gtmpl")
   Template index;
+/*
+  @Inject
+  OrganizationService organizationService;
+  */
+  @Inject
+  IService brandAdvocacyService;
+
+  @Inject
+  //OrganizationService organizationService,
+  public JuZBackEndApplication(IService brandAdvocacyService){
+    //this.organizationService = organizationService;
+    this.brandAdvocacyService = brandAdvocacyService;
+  }
 
   @View
   public Response.Content index(){
-    return index("i_am_urlparams");
+    return index.ok();
   }
-  @Inject
-  Service service;
 
-  @View
-  @Route("/show/{location}")
-  public Response.Content index(String location){
-    Map<String,Object> parameters = new HashMap<String, Object>();
-    String serviceString = service.show();
-    parameters.put("urlParams",location);
-    parameters.put("serviceResult",serviceString);
-    parameters.put("location", "marseille");
-    parameters.put("temperature", serviceString);
-    parameters.put("locations", locations);
-    return index.with(parameters).ok();
-  }
 
   @Action
-  @Route("/add")
-  public Response.View add(String location){
-    locations.add(location);
-    return JuZBackEndApplication_.index(location);
+  @Route("/addMission")
+  public Response.View addMission(String title){
+    Mission mission = new Mission(title);
+    this.brandAdvocacyService.addMission(mission);
+    return JuZBackEndApplication_.index();
   }
+
 }
--- webapp/src/main/java/org/exoplatform/community/brandadvocacy/portlet/backend/package-info.java
+++ webapp/src/main/java/org/exoplatform/community/brandadvocacy/portlet/backend/package-info.java
@@ -3,9 +3,18 @@
  */
 @Application(defaultController = JuZBackEndApplication.class)
 @Portlet(name="BackendPortlet")
-@Servlet("/portlet/")
+@Bindings(
+  {
+//    @Binding(value = org.exoplatform.services.organization.OrganizationService.class),
+    @Binding(value = org.exoplatform.brandadvocacy.service.IService.class,implementation = org.exoplatform.brandadvocacy.service.JCRImpl.class)
+  }
+)
+@Servlet("/")
 package org.exoplatform.community.brandadvocacy.portlet.backend;
 
 import juzu.Application;
+import juzu.plugin.binding.Binding;
+import juzu.plugin.binding.Bindings;
 import juzu.plugin.portlet.Portlet;
 import juzu.plugin.servlet.Servlet;
+import org.exoplatform.brandadvocacy.service.IService;
